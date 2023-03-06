00705a25bbfbd5d0d4e0718d8ea2a82274f9dc28
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-03-06 15:09:26 +0200

    import

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,10 +1,11 @@
 const getCurrent = require('./getCurrent');
 const login = require('./login');
 const logout = require('./logout');
-
+const signup = require('./signup');
 
 module.exports = {
   getCurrent,
   login,
   logout,
+  signup,
 };
