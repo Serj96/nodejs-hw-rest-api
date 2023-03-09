4552417978caeb1b1af046d1648750be865b4c7a
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-03-09 13:50:49 +0200

    check password

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,6 +1,6 @@
 const { Conflict } = require('http-errors');
 const { User } = require('../../models/user');
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 
 const signup = async (req, res) => {
   const { email, password, subscription } = req.body;
@@ -8,14 +8,16 @@ const signup = async (req, res) => {
   if (user) {
     throw new Conflict(`409, Email ${email} in use`);
   }
+    
+ // метод для перевірки пароля
 
-//   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-//   const result = await User.create({
-//     email,
-//     password: hashPassword,
-//     subscription,
-//   });
+  const result = await User.create({
+    email,
+    password: hashPassword,
+    subscription,
+  });
 
   res.status(201).json({
     Status: 'success',
