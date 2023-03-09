311980cfe8882e71d7e1200eda2aba8b1a1cb8f5
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-03-09 12:35:12 +0200

    bag fix

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose
+  .set('strictQuery', false)
   .connect(DB_HOST)
   .then(() => app.listen(PORT))
   .catch(error => {
