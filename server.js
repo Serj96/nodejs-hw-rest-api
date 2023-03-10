74db00573f96fe62906f995544df9da904bcf653
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-03-10 10:24:34 +0200

    bag fix

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ const app = require('./app');
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
+mongoose.set('strictQuery', true);
+
 mongoose
-  .set('strictQuery', false)
   .connect(DB_HOST)
-  .then(() => app.listen(PORT))
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log('Database connection successful');
+    })
+  )
   .catch(error => {
     console.log(error.message);
     process.exit(1);
