fc4f624b3da13b26d12935dca3d91e7f4431fca6
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-03-15 10:36:26 +0200

    remove subscrip

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,12 +2,10 @@ const getCurrent = require('./getCurrent');
 const login = require('./login');
 const logout = require('./logout');
 const signup = require('./signup');
-const updateSubscription = require('./updateSubscription');
 
 module.exports = {
   getCurrent,
   login,
   logout,
   signup,
-  updateSubscription,
 };
