77b13845e6673ad026bf0b8e42689f1bfaab9d09
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-03-15 15:35:58 +0200

    bag fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,13 +8,10 @@ const { SECRET_KEY } = process.env;
 const auth = async (req, res, next) => {
     const { authorization = " "} = req.headers;
     const [bearer, token] = authorization.split('');
-    console.log(authorization);
-
     try {
         if (bearer !== "Bearer" || !token) {
             throw new Unauthorized("Not authorized");
         }
-
         const { id } = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id)
         if (!user || user.token !== token) {
